83c22d75ba3f17e52dc6e4dd2e2c2fc1fa39f980
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-09-28 21:26:44 -0700

    Init Amigoes Backend with typescript

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Typescript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
