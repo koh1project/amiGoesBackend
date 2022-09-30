62c0e5fa613e2b4b28253ef8ad5133beffa4468c
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-09-29 22:34:29 -0700

    Feat: Setup Eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
 });
-
 const uri = process.env.DATABASE_URI;
 
+// eslint-disable-next-line promise/prefer-await-to-callbacks
 mongoose.connect(uri, (error) => console.log(error));
