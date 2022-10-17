d2d35fc41181fd1fd1530abb2c778e64022c695f
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-10-17 15:27:22 -0700

    feature: text from image - add client

---
+++ src/utils/rekognitionUtils.ts
@@ -0,0 +1,26 @@
+import { DetectTextCommand } from '@aws-sdk/client-rekognition';
+import { rekognitionClient } from '../libs/rekognitionClient';
+
+export const recognizeText = async (image) => {
+  const params = {
+    Image: {
+      S3Object: {
+        Bucket: process.env.S3_BUCKET,
+        Name: image,
+      },
+    },
+  };
+
+  try {
+    const data = await rekognitionClient.send(new DetectTextCommand(params));
+    const textDetections = data.TextDetections;
+    textDetections.forEach((text) => {
+      if (text.Type === 'LINE') {
+        console.log(text.DetectedText);
+      }
+    });
+    return data;
+  } catch (err) {
+    console.log('Error', err);
+  }
+};
