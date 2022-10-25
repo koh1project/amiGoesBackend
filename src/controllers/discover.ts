f889a8a4be69a1ec18dc2c46ca032c36613dce95
Koichi <koichi89592@gmail.com>
2022-10-25 16:22:41 -0700

    [add] replace aws location to google map

--- src/controllers/discover.ts
+++ src/controllers/discover.ts
@@ -1,23 +1,23 @@
 import { Request, Response } from 'express';
-import { locationClient } from '../libs/locationClient';
+
+import { Client } from '@googlemaps/google-maps-services-js';
+const client = new Client({});
 
 export const fetchPlacesByKeyword = async (
   req: Request<{ keyword?: string }>,
   res: Response,
 ) => {
   try {
-    const keyword: string = req.query?.keyword?.toString() || 'park vancouver';
+    // const keyword: string = req.query?.keyword?.toString() || 'park vancouver';
 
-    const params = {
-      IndexName: 'explore.place',
-      Text: keyword,
-      BiasPosition: [-123.11589890095398, 49.295868390331194],
-      MaxResults: 20,
-    };
+    const parksResponse = await client.textSearch({
+      params: {
+        query: 'parks vancouver',
+        key: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    });
 
-    const result = await locationClient.searchPlaceIndexForText(params);
-
-    res.status(200).json(result);
+    res.status(200).json(parksResponse.data);
   } catch (error) {
     console.log('error: ', error);
     res.status(error['$metadata'].httpStatusCode).json(error);
