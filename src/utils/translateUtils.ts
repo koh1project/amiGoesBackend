34ddbdaa2a6896fc81af576572825a7afc44b2ac
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-10-24 23:20:02 -0700

    feat: translate text

---
+++ src/utils/translateUtils.ts
@@ -0,0 +1,18 @@
+import { TranslateTextCommand } from '@aws-sdk/client-translate';
+import { translateClient } from '../libs/translateClient';
+
+export const translateText = async (language, text) => {
+  const params = {
+    SourceLanguageCode: 'auto',
+    TargetLanguageCode: language,
+    Text: text,
+  };
+
+  try {
+    const data = await translateClient.send(new TranslateTextCommand(params));
+    console.log('Success', data.TranslatedText);
+    return data;
+  } catch (err) {
+    console.log('Error', err);
+  }
+};
