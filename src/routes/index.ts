da7e921317d4a80ebe431f8e7acf895e92ef420b
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-10-24 23:20:02 -0700

    feat: translate text

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -51,3 +51,4 @@ router.post('/translateImage', async (req, res) => {
   const translatedResult = await translateText(req.body.language, data.TextDetections[0].DetectedText);
 
 export { router };
+
