8c20c75879e1379f3ccd8736a5b2d3ae31b69c38
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-10-27 23:07:22 -0700

    fix: translate text return

--- src/utils/translateUtils.ts
+++ src/utils/translateUtils.ts
@@ -11,7 +11,7 @@ export const translateText = async (language, text) => {
   try {
     const data = await translateClient.send(new TranslateTextCommand(params));
     console.log('Success', data.TranslatedText);
-    return data;
+    return data.TranslatedText;
   } catch (err) {
     console.log('Error', err);
   }
