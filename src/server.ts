c830696e1554b82cab9b6c3cc377a38640eaef6d
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-11-04 01:32:56 -0700

    fix: request limit

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(router);
 
 app.get('/', (req: Request, res: Response) => {
