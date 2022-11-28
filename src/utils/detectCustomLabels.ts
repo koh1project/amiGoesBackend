64886e9721a28fcb4c1d7daa468b7d5f5a18ad23
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-11-28 00:11:26 -0800

    feat: id verification custom labels

---
+++ src/utils/detectCustomLabels.ts
@@ -0,0 +1,31 @@
+import {
+  DetectCustomLabelsCommand,
+  RekognitionClient,
+} from '@aws-sdk/client-rekognition';
+import { REGION } from '../libs/s3Client';
+
+const client = new RekognitionClient({ region: REGION });
+
+export const detectCustomLabels = async (image) => {
+  console.log('backend');
+  const buffer = Buffer.from(image, 'base64');
+  console.log(buffer);
+  const params = {
+    Image: {
+      Bytes: buffer,
+    },
+    MinConfidence: 70,
+    ProjectVersionArn:
+      'arn:aws:rekognition:us-west-2:834021575386:project/amigoesCustomLabels/version/amigoesCustomLabels.2022-10-27T18.35.09/1666920909885',
+  };
+
+  const command = new DetectCustomLabelsCommand(params);
+
+  try {
+    const data = await client.send(command);
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log('Error', err);
+  }
+};
